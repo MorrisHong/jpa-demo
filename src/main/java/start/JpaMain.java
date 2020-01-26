6a03283a0fe89f7d2960df3fe3a649f10f63a499
gracelove <govlmo91@gmail.com>
2020-01-26 15:25:34 +0900

    member 객체 영속성 crud

---
+++ src/main/java/start/JpaMain.java
@@ -0,0 +1,60 @@
+package start;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+
+public class JpaMain {
+    public static void main(String[] args) {
+        //META-INF/persistance.xml의 유닛 이름을 인자로 넘겨준다.
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            /**
+             * 비즈니스로직
+             */
+            logic(em);
+            tx.commit();
+        }catch (Exception e) {
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+    private static void logic(EntityManager em) {
+        String id = "id1";
+        Member member = new Member();
+        member.setId(id);
+        member.setUsername("gracelove");
+        member.setAge(30);
+
+        //등록
+        em.persist(member);
+
+        //수정
+        member.setAge(20);
+
+        //한건조회
+        Member findMember = em.find(Member.class, id);
+        System.out.println("findMemberName = " + findMember.getUsername());
+        System.out.println("findMemberAge = " + findMember.getAge());
+
+        //목록조회
+        List<Member> members = em.createQuery("select m from Member as m", Member.class)
+                .getResultList();
+        System.out.println("members.size = " + members.size());
+
+        //삭제
+        em.remove(member);
+    }
+}
